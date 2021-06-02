e9cab6d81546580cb1f3b983bef4e43a17a7044c
Jacobo de Haro <jacobodeharo@gmail.com>
2021-06-02 13:48:23 +0200

    Minimal project reproducing issue in Vite 2.3.0 - 2.3.2

---
+++ README.md
@@ -0,0 +1,7 @@
+# vite-preact-ts-primereact
+
+This is a minimal project to reproduce [an issue](https://github.com/vitejs/vite/issues/3437) in vite@2.3.0 - vite@2.3.2.
+
+Steps:
+1. `npm i`
+2. `npm run dev`
--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,4 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default ({ command, mode }: ConfigEnv): UserConfig => ({
     plugins: [preact()],
-    esbuild: {
-      jsxFactory: 'h',
-      jsxFragment: 'Fragment',
-      jsxInject: `import 'preact/debug';`,
-    },
-    resolve: {
-      alias: {
-        'react': 'preact-compat',
-        'react-dom': 'preact-compat',
-        'create-react-class': 'preact-compat/lib/create-react-class',
-        'react-dom-factories': 'preact-compat/lib/react-dom-factories',
-      },
-    },
 });
